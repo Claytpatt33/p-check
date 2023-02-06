358a6806fdb588cb320171404eab13e1f6afdb72
Claytpatt33 <124512575+Claytpatt33@users.noreply.github.com>
2023-02-06 14:25:58 -0500

    New index.js

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+  const [startBlock, setStartBlock] = useState('');
+  const [endBlock, setEndBlock] = useState('');
+  const [transfers, setTransfers] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch(`/check?startBlock=${startBlock}&endBlock=${endBlock}`);
+    const data = await response.json();
+
+    setTransfers(data.transfers);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={startBlock}
+          onChange={(e) => setStartBlock(e.target.value)}
+          placeholder="Start block"
+        />
+        <input
+          type="text"
+          value={endBlock}
+          onChange={(e) => setEndBlock(e.target.value)}
+          placeholder="End block"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {transfers && Object.keys(transfers).length > 0 && (
+        <ul>
+          {Object.entries(transfers).map(([address, transferCount]) => (
+            <li key={address}>
+              {address}: {transferCount} transfers
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Home;
