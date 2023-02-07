5eb663b51973a60f3e7741f64a4b91ce0cfeecf7
Claytpatt33 <124512575+Claytpatt33@users.noreply.github.com>
2023-02-06 19:23:10 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,23 +24,26 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={startBlock} onChange={e => setStartBlock(e.target.value)} />
-        <input type="text" value={endBlock} onChange={e => setEndBlock(e.target.value)} />
-        <button type="submit">Submit</button>
-      </form>
-      {error && <div>Error: {error.message}</div>}
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        Object.keys(transfers).map(address => (
-          <div key={address}>
-            Address: {address}
-            Number of Transfers: {transfers[address]}
-          </div>
-        ))
-      )}
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div>
+        <div style={{ textAlign: 'center' }}>Pudgy Penguins Transfer Checker</div>
+        <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <input type="text" value={startBlock} onChange={e => setStartBlock(e.target.value)} />
+          <input type="text" value={endBlock} onChange={e => setEndBlock(e.target.value)} />
+          <button type="submit">Submit</button>
+        </form>
+        {error && <div>Error: {error.message}</div>}
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          Object.keys(transfers).map(address => (
+            <div key={address}>
+              Address: {address}
+              Number of Transfers: {transfers[address]}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
