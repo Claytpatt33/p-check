9141ca7a38bfb4112d3648cc2170a83240614ed8
Claytpatt33 <124512575+Claytpatt33@users.noreply.github.com>
2023-02-06 21:06:21 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,27 +8,20 @@ const Home = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const headers = {
-    'Content-Type': 'application/json',
-  };
+  'Content-Type': 'application/json',
+};
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
 
     try {
-      const response = await fetch(
-        `https://api.etherscan.io/api?module=account&action=tokentx&address=0x123456&startblock=${startBlock}&endblock=${endBlock}&sort=asc&apikey=YourApiKey`,
-        {
-          headers: headers
-        }
-      );
+      const response = await fetch("https://mainnet.infura.io/v3/b41746a63ed848c683d56bf98c5e5212");
       
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      
+      console.log(response);
       const data = await response.json();
-      setTransfers(data.result);
+      console.log(data)
+      setTransfers(data.transfers);
     } catch (err) {
       setError(err);
     } finally {
@@ -41,27 +34,20 @@ const Home = () => {
       <div>
         <div style={{ textAlign: 'center' }}>Pudgy Penguins Transfer Checker</div>
         <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <input type="text" value={startBlock} onChange={e => setStartBlock(e.target.value)} placeholder="Start Block" />
-          <input type="text" value={endBlock} onChange={e => setEndBlock(e.target.value)} placeholder="End Block" />
+          <input type="text" value={startBlock} onChange={e => setStartBlock(e.target.value)} />
+          <input type="text" value={endBlock} onChange={e => setEndBlock(e.target.value)} />
           <button type="submit">Submit</button>
         </form>
         {error && <div>Error: {error.message}</div>}
         {isLoading ? (
           <div>Loading...</div>
         ) : (
-          <ul>
-            {transfers.map(transfer => (
-              <li key={transfer.hash}>
-                Block: {transfer.blockNumber}
-                Hash: {transfer.hash}
-                From: {transfer.from}
-                To: {transfer.to}
-                Token Name: {transfer.tokenName}
-                Value: {transfer.value}
-                Timestamp: {transfer.timeStamp}
-              </li>
-            ))}
-          </ul>
+          Object.keys(transfers).map(address => (
+            <div key={address}>
+              Address: {address}
+              Number of Transfers: {transfers[address]}
+            </div>
+          ))
         )}
       </div>
     </div>
