1247272f39d20789d7376683d0476486ab267385
Claytpatt33 <124512575+Claytpatt33@users.noreply.github.com>
2023-02-07 13:01:22 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,57 @@
-const { ethers } = require("ethers");
-const provider = new ethers.providers.InfuraProvider.getWebSocketProvider(
-  "mainnet",
-  "b41746a63ed848c683d56bf98c5e5212"
-);
+import React, { useState } from 'react';
+import Web3 from 'web3';
 
-async function getTransactionCountInBlockRange(startBlock, endBlock) {
-  let transactionCount = 0;
-  for (let blockNumber = startBlock; blockNumber <= endBlock; blockNumber++) {
-    const block = await provider.getBlock(blockNumber);
-    transactionCount += block.transactions.length;
-  }
-  console.log(`Number of transactions in block range [${startBlock}, ${endBlock}]: ${transactionCount}`);
-}
+const Home = () => {
+  const [startBlock, setStartBlock] = useState('');
+  const [endBlock, setEndBlock] = useState('');
+  const [transfers, setTransfers] = useState({});
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const headers = {
+  'Content-Type': 'application/json',
+};
 
-const startBlock = parseInt(prompt("Enter thestart block number: "));
-const endBlock = parseInt(prompt("Enter the end block number: "));
-getTransactionCountInBlockRange(startBlock, endBlock);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
 
+    try {
+      const response = await fetch("https://mainnet.infura.io/v3/b41746a63ed848c683d56bf98c5e5212");
+      
+      console.log(response);
+      const data = await response.json();
+      console.log(data)
+      setTransfers(data.transfers);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div>
+        <div style={{ textAlign: 'center' }}>Pudgy Penguins Transfer Checker</div>
+        <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <input type="text" value={startBlock} onChange={e => setStartBlock(e.target.value)} />
+          <input type="text" value={endBlock} onChange={e => setEndBlock(e.target.value)} />
+          <button type="submit">Submit</button>
+        </form>
+        {error && <div>Error: {error.message}</div>}
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          Object.keys(transfers).map(address => (
+            <div key={address}>
+              Address: {address}
+              Number of Transfers: {transfers[address]}
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
